0c98a6d741be4a271f8b51d92b5805e8baca50c3
unknown <14913130+phillram@users.noreply.github.com>
2020-04-12 12:18:26 -0700

    Reworked the script to send a DM, include a hyperlink, and to listen for some phrases

--- app.py
+++ app.py
@@ -1,150 +1,73 @@
-import os
-import logging
+# Importing requirements 
+# _______________________________________________________________
+import os, logging, certifi
+# import logging
 from flask import Flask
 from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 import ssl as ssl_lib
-import certifi
-from onboarding_tutorial import OnboardingTutorial
+# import certifi
+# from onboarding_tutorial import OnboardingTutorial
 
-
-
+# Initializing everything
+# _______________________________________________________________
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
-slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], "/slack/events", app)
+slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
 
 # Initialize a Web API client
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
-
-onboarding_tutorials_sent = {}
-
-
-def start_onboarding(user_id: str, channel: str):
-    # Create a new onboarding tutorial.
-    onboarding_tutorial = OnboardingTutorial(channel)
-
-    # Get the onboarding message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the onboarding message in Slack
-    response = slack_web_client.chat_postMessage(**message)
-
-    # Capture the timestamp of the message we've just posted so
-    # we can use it to update the message after a user
-    # has completed an onboarding task.
-    onboarding_tutorial.timestamp = response["ts"]
-
-    # Store the message sent in onboarding_tutorials_sent
-    if channel not in onboarding_tutorials_sent:
-        onboarding_tutorials_sent[channel] = {}
-    onboarding_tutorials_sent[channel][user_id] = onboarding_tutorial
-
-# ================ Team Join Event =============== #
-# When the user first joins a team, the type of the event will be 'team_join'.
-# Here we'll link the onboarding_message callback to the 'team_join' event.
-@slack_events_adapter.on("team_join")
-def onboarding_message(payload):
-    """Create and send an onboarding welcome message to new users. Save the
-    time stamp of this message so we can update this message in the future.
-    """
-    event = payload.get("event", {})
-
-    # Get the id of the Slack user associated with the incoming event
-    user_id = event.get("user", {}).get("id")
-
-    # Open a DM with the new user.
-    response = slack_web_client.im_open(user_id)
-    channel = response["channel"]["id"]
-
-    # Post the onboarding message.
-    start_onboarding(user_id, channel)
+# The words that Grievous bot will listen for
+phrases = ['zoop', 'zeep', 'mirage']
 
 
-# ============= Reaction Added Events ============= #
-# When a users adds an emoji reaction to the onboarding message,
-# the type of the event will be 'reaction_added'.
-# Here we'll link the update_emoji callback to the 'reaction_added' event.
-@slack_events_adapter.on("reaction_added")
-def update_emoji(payload):
-    """Update the onboarding welcome message after receiving a "reaction_added"
-    event from Slack. Update timestamp for welcome message as well.
-    """
-    event = payload.get("event", {})
-
-    channel_id = event.get("item", {}).get("channel")
-    user_id = event.get("user")
-
-    if channel_id not in onboarding_tutorials_sent:
-        return
-
-    # Get the original tutorial sent.
-    onboarding_tutorial = onboarding_tutorials_sent[channel_id][user_id]
-
-    # Mark the reaction task as completed.
-    onboarding_tutorial.reaction_task_completed = True
-
-    # Get the new message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the updated message in Slack
-    updated_message = slack_web_client.chat_update(**message)
-
-    # Update the timestamp saved on the onboarding tutorial object
-    onboarding_tutorial.timestamp = updated_message["ts"]
-
-
-# =============== Pin Added Events ================ #
-# When a users pins a message the type of the event will be 'pin_added'.
-# Here we'll link the update_pin callback to the 'reaction_added' event.
-@slack_events_adapter.on("pin_added")
-def update_pin(payload):
-    """Update the onboarding welcome message after receiving a "pin_added"
-    event from Slack. Update timestamp for welcome message as well.
-    """
-    event = payload.get("event", {})
-
-    channel_id = event.get("channel_id")
-    user_id = event.get("user")
-
-    # Get the original tutorial sent.
-    onboarding_tutorial = onboarding_tutorials_sent[channel_id][user_id]
-
-    # Mark the pin task as completed.
-    onboarding_tutorial.pin_task_completed = True
-
-    # Get the new message payload
-    message = onboarding_tutorial.get_message_payload()
-
-    # Post the updated message in Slack
-    updated_message = slack_web_client.chat_update(**message)
-
-    # Update the timestamp saved on the onboarding tutorial object
-    onboarding_tutorial.timestamp = updated_message["ts"]
-
-
-# ============== Message Events ============= #
-# When a user sends a DM, the event type will be 'message'.
-# Here we'll link the message callback to the 'message' event.
-@slack_events_adapter.on("message")
+# Message events will trigger when a user types in a message
+# _______________________________________________________________
+@slack_events_adapter.on('message')
 def message(payload):
-    """Display the onboarding welcome message after receiving a message
-    that contains "start".
-    """
-    event = payload.get("event", {})
+    
+    # Stores information about the message
+    event = payload.get('event', {})
+    
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+    timestamp = event.get('ts')
+    # print('>>> channel_id is: ' + channel_id)
+    # print('>>> user_id is: ' + user_id)
+    # print('>>> text is: ' + text)
+    # print('>>> timestamp is: ' + str(timestamp))
 
-    channel_id = event.get("channel")
-    user_id = event.get("user")
-    text = event.get("text")
+    # Checks if the message contains any of the words from the phrases list
+    if text.lower() in phrases:
 
+        # Opens a DM with user and saves the channel ID
+        response = slack_web_client.im_open( user = user_id)
+        dm_channel = response['channel']['id']
+        # print('>>> im_channel is: ' + str(im_channel))
+        
+        # Uses the user ID to pull the display name
+        # of the user that triggered the bot
+        response = slack_web_client.users_info(user = user_id)
+        triggering_users_name = response['user']['profile']['display_name']
 
-    if text and text.lower() == "start":
-        return start_onboarding(user_id, channel_id)
+        # Uses the timestamp to generate a link to the message
+        response = slack_web_client.chat_getPermalink(channel = channel_id, message_ts = timestamp)
+        message_link = response['permalink']
+        # print('>>> message_link is: ' + str(message_link))
 
+        # Sends the message to the IM channel above
+        slack_web_client.chat_postMessage(
+            channel = dm_channel,
+            text = triggering_users_name + ' mentioned your keyword! \n' + message_link
+        )
 
-if __name__ == "__main__":
+# Start Grievous bot and run flask on port 5066
+# _______________________________________________________________
+if __name__ == '__main__':
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
     ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
-    app.run(port=3000)
+    app.run(port=5066)

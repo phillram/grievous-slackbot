7a24b11046b24bb744b002827905e71bad488270
unknown <14913130+phillram@users.noreply.github.com>
2020-04-12 17:32:22 -0700

    Changed the data model to a dictionary. Only ping requested users

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import os, logging, certifi
 from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 from flask import Flask, request
-# from flask_sqlalchemy import SQLAlchemy
 import ssl as ssl_lib
+from collections import defaultdict 
 
 # _______________________________________________________________
 # Initializing everything
@@ -15,27 +15,15 @@ import ssl as ssl_lib
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///keywords.db'
-# db = SQLAlchemy(app)
-
-# # Initialize the flask database
-# class Keywords(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     slack_user_id = db.Column(db.String(25), nullable = False)
-#     slack_keyword = db.Column(db.String(100), nullable = False)
-
-#     def __repr__(self):
-#         return '<Task %r>' % self.id
-
 # Initialize a connection to Slack events
 slack_events_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
 
 # Initialize a Web API client
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
-# The words that Grievous bot will listen for
-users = []
-phrases_list = []
+# Initialize the words that Greivous bot will listen for
+word_list = defaultdict(list)
+
 
 
 # _______________________________________________________________
@@ -48,8 +36,6 @@ phrases_list = []
 # Slack should automatically enlarge the gif
 @app.route('/slack/hellothere', methods=['POST'])
 def hello_there():
-    data = request.get_data()
-    print(data)
     return('https://media.giphy.com/media/7JC7bCJJGj44aBwB8p/giphy.gif')
 
 
@@ -59,11 +45,12 @@ def hello_there():
 @app.route('/slack/watch', methods=['POST'])
 def add_to_watchlist():
     phrase = request.form['text']
-    # print('>>> The given phrase is: ' + phrase)
+    user_id = request.form['user_id']
+    # print('The user is: >' + user_id + '< The phrase is >' + phrase + '<')
 
-    phrases_list.append(phrase.lower())
-    return('Phrase added. Your current list is: ' + str(phrases_list)[1:-1])
-
+    # Adds the word to the requesters list
+    word_list[user_id].append(phrase.lower())
+    return('Phrase added. Your current list is: ' + str(word_list[user_id])[1:-1])
 
 # For '/dontwatch' command. Removes the given words to the watchlist.
 # This takes in the word provided by the user, removes it, and then
@@ -71,22 +58,34 @@ def add_to_watchlist():
 @app.route('/slack/dontwatch', methods=['POST'])
 def remove_from_watchlist():
     phrase = request.form['text']
-    # print('>>> The given phrase is: ' + phrase)
+    user_id = request.form['user_id']
+    # print('The user is: >' + user_id + '< The phrase is >' + phrase + '<')
 
-    phrases_list.remove(phrase.lower())
-    if not phrases_list:
+    # Tries to remove the phrase they said. Will return an error
+    # message if unable to do so
+    try:
+        word_list[user_id].remove(phrase.lower())
+    except:
+        return('You aren\'t watching anything yet. \n Add some with /watch')
+
+    # Checks if the user has any phrases left. And, will display
+    # if they have more
+    if not word_list[user_id]:
         return('Phrase removed. Your current list is empty')
     else:
-        return('Phrase removed. Your current list is: ' + str(phrases_list)[1:-1])
-
+        return('Phrase removed. Your current list is: ' + str(word_list[user_id])[1:-1])
 
 # For '/watchlist' command. Displays the current watchlist.
 @app.route('/slack/watchlist', methods=['POST'])
 def show_watchlist():
-    if not phrases_list:
+    user_id = request.form['user_id']
+    
+    # Checks if the user is watching for any phrases. If not then return 
+    # an error message
+    if not word_list[user_id]:
         return('You aren\'t watching anything yet. \n Add some with /watch')
     else:
-        return('You are currently watching for: ' + str(phrases_list)[1:-1])
+        return('Your current watchlist is: ' + str(word_list[user_id])[1:-1])
 
 
 # _______________________________________________________________
@@ -108,30 +107,37 @@ def message(payload):
     # print('>>> text is: ' + text)
     # print('>>> timestamp is: ' + str(timestamp))
 
-    # Checks if the message contains any of the words from the phrases_list list
-    # then sends the user a DM with a link to the message
-    if any(word in text.lower() for word in phrases_list):
-
-        # Opens a DM with user and saves the channel ID
-        response = slack_web_client.im_open( user = user_id)
-        dm_channel = response['channel']['id']
-        # print('>>> im_channel is: ' + str(im_channel))
+    # This looks through all the values in word_list in order to match
+    # against the word_list values
+    # It then finds the user who added the word, and messages them
+    for key, value in word_list.items():
+        # print('\n\n\n\nyou are in the loop')
+        # print('key is: ' + str(key) + ' And value is: ' + str(value))
+        # print('>>> text is: ' + text)
         
-        # Uses the user ID to pull the display name
-        # of the user that triggered the bot
-        response = slack_web_client.users_info(user = user_id)
-        triggering_users_name = response['user']['profile']['display_name']
+        # Checking if the word exists in the word_list
+        if any(word in text.lower() for word in value):
 
-        # Uses the timestamp to generate a link to the message
-        response = slack_web_client.chat_getPermalink(channel = channel_id, message_ts = timestamp)
-        message_link = response['permalink']
-        # print('>>> message_link is: ' + str(message_link))
+            # Opens a DM with to the requesting user and saves the channel ID
+            response = slack_web_client.im_open( user = key)
+            dm_channel = response['channel']['id']
+            # print('>>> im_channel is: ' + str(im_channel))
+            
+            # Uses the user ID to pull the display name
+            # of the user that triggered the bot
+            response = slack_web_client.users_info(user = user_id)
+            triggering_users_name = response['user']['profile']['display_name']
 
-        # Sends the message to the IM channel above
-        slack_web_client.chat_postMessage(
-            channel = dm_channel,
-            text = triggering_users_name + ' mentioned your keyword! \n' + message_link
-        )
+            # Uses the timestamp to generate a link to the message
+            response = slack_web_client.chat_getPermalink(channel = channel_id, message_ts = timestamp)
+            message_link = response['permalink']
+            # print('>>> message_link is: ' + str(message_link))
+
+            # Sends the message to the IM channel above
+            slack_web_client.chat_postMessage(
+                channel = dm_channel,
+                text = triggering_users_name + ' mentioned your keyword! \n' + message_link
+            )
 
 
 
